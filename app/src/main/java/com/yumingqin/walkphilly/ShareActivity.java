ff85e9ec5271d99792b80ba2eef48ecacb6e9bb0
ejherzog <evajherzog@gmail.com>
2017-02-25 16:13:49 -0500

    social media

---
+++ app/src/main/java/com/yumingqin/walkphilly/ShareActivity.java
@@ -0,0 +1,24 @@
+package com.yumingqin.walkphilly;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by eherzog on 2/25/2017.
+ */
+
+public class ShareActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+
+
+
+
+    }
+
+
+}
+
+
