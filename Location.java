fed8a0a63eec0601acee197110ccbd93799a81db
rrehman93 <rrehman@seas.upenn.edu>
2017-02-25 14:31:20 -0500

    Add files via upload

---
+++ Location.java
@@ -0,0 +1,30 @@
+
+public class Location {
+	private double lat;
+	private double longit;
+	private String name;
+	private String description;
+	
+	public Location(double lattitude, double longitude, String locName, String description){
+		lat = lattitude;
+		longit = longitude;
+		name = locName;
+		this.description = description;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public double getLat(){
+		return lat;
+	}
+	
+	public double getLongit(){
+		return longit;
+	}
+	
+	public String getName(){
+		return name;
+	}
+}
